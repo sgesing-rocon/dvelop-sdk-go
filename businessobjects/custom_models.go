4da0190591b2038adea72a8a5be328ae0aff6667
Lennart Klose <lenklose+github@gmail.com>
2023-01-30 09:50:29 +0100

    Change structnames

--- businessobjects/custom_models.go
+++ businessobjects/custom_models.go
@@ -13,46 +13,96 @@ import (
 	"strconv"
 )
 
-type FullCustomModelDto struct {
+type Key struct {
 	Id          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	State       string `json:"state"`
-	EntityTypes []struct {
-		PluralName  string `json:"pluralName"`
-		Id          string `json:"id"`
-		Name        string `json:"name"`
-		Description string `json:"description"`
-		State       string `json:"state"`
-		Key         struct {
-			Id          string `json:"id"`
-			Name        string `json:"name"`
-			Description string `json:"description"`
-			State       string `json:"state"`
-			Type        string `json:"type"`
-		} `json:"key"`
-		Properties []struct {
-			Id          string `json:"id"`
-			Name        string `json:"name"`
-			Description string `json:"description"`
-			State       string `json:"state"`
-			Type        string `json:"type"`
-			Required    bool   `json:"required"`
-			Indexed     bool   `json:"indexed"`
-		} `json:"properties"`
-	} `json:"entityTypes"`
+	Type        string `json:"type"`
 }
 
-type customModelList struct {
-	Items []FullCustomModelDto `json:"value"`
+type Property struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	State       string `json:"state"`
+	Type        string `json:"type"`
+	Required    bool   `json:"required"`
+	Indexed     bool   `json:"indexed"`
 }
 
-type GetCustomModelsRequest struct {
+type EntityType struct {
+	PluralName  string     `json:"pluralName"`
+	Id          string     `json:"id"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	State       string     `json:"state"`
+	Key         Key        `json:"key"`
+	Properties  []Property `json:"properties"`
+}
+
+type CustomModel struct {
+	Id          string       `json:"id"`
+	Name        string       `json:"name"`
+	Description string       `json:"description"`
+	State       string       `json:"state"`
+	EntityTypes []EntityType `json:"entityTypes"`
+}
+
+type getCustomModelsResponseDto struct {
+	Items []CustomModel `json:"value"`
+}
+
+type GetCustomModelRequest struct {
+	SystemBaseUri string
+	AuthSessionId string
+	ModelId       string
+}
+
+func (c *DefaultClient) GetCustomModel(ctx context.Context, request GetCustomModelRequest) (CustomModel, error) {
+	baseUri, authSessionId, err := c.getContextValues(ctx, request.SystemBaseUri, request.AuthSessionId)
+	if err != nil {
+		return CustomModel{}, err
+	}
+
+	uri, err := url.Parse(fmt.Sprintf("%v/businessobjects/core/models/customModels(%v)", baseUri, request.ModelId))
+	if err != nil {
+		return CustomModel{}, fmt.Errorf("error parsing raw url with base uri '%v' - error: %v", baseUri, err.Error())
+	}
+
+	req := &http.Request{
+		Method: http.MethodGet,
+		URL:    uri,
+		Header: map[string][]string{
+			"Accept":        {"application/json"},
+			"Authorization": {"Bearer " + authSessionId},
+		},
+	}
+
+	response, err := c.HttpClient.Do(req)
+	if err != nil {
+		log.Fatalf("error sending request to business objects. %+v", err)
+	}
+	if !isSuccessStatusCode(response.StatusCode) {
+		return CustomModel{}, errors.New("http request failed with status code: " + strconv.Itoa(response.StatusCode))
+	}
+	defer response.Body.Close()
+
+	model := CustomModel{}
+	err = json.NewDecoder(response.Body).Decode(&model)
+	if err != nil {
+		return CustomModel{}, fmt.Errorf("error parsing response from business objects get custom models - error: %v", err.Error())
+	}
+
+	return model, nil
+}
+
+type ListCustomModelsRequest struct {
 	SystemBaseUri string
 	AuthSessionId string
 }
 
-func (c *DefaultClient) GetCustomModels(ctx context.Context, request GetCustomModelsRequest) ([]FullCustomModelDto, error) {
+func (c *DefaultClient) ListCustomModels(ctx context.Context, request ListCustomModelsRequest) ([]CustomModel, error) {
 	baseUri, authSessionId, err := c.getContextValues(ctx, request.SystemBaseUri, request.AuthSessionId)
 	if err != nil {
 		return nil, err
@@ -81,7 +131,7 @@ func (c *DefaultClient) GetCustomModels(ctx context.Context, request GetCustomMo
 	}
 	defer response.Body.Close()
 
-	modelList := customModelList{}
+	modelList := getCustomModelsResponseDto{}
 	err = json.NewDecoder(response.Body).Decode(&modelList)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing response from business objects list custom models - error: %v", err.Error())
@@ -90,71 +140,33 @@ func (c *DefaultClient) GetCustomModels(ctx context.Context, request GetCustomMo
 	return modelList.Items, nil
 }
 
-type GetCustomModelRequest struct {
-	SystemBaseUri string
-	AuthSessionId string
-	ModelId       string
+type CreateCustomModelDtoProperty struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+	Required    bool   `json:"required"`
+	Indexed     bool   `json:"indexed"`
 }
 
-func (c *DefaultClient) GetCustomModel(ctx context.Context, request GetCustomModelRequest) (FullCustomModelDto, error) {
-	baseUri, authSessionId, err := c.getContextValues(ctx, request.SystemBaseUri, request.AuthSessionId)
-	if err != nil {
-		return FullCustomModelDto{}, err
-	}
+type CreateCustomModelDtoKey struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+}
 
-	uri, err := url.Parse(fmt.Sprintf("%v/businessobjects/core/models/customModels(%v)", baseUri, request.ModelId))
-	if err != nil {
-		return FullCustomModelDto{}, fmt.Errorf("error parsing raw url with base uri '%v' - error: %v", baseUri, err.Error())
-	}
-
-	req := &http.Request{
-		Method: http.MethodGet,
-		URL:    uri,
-		Header: map[string][]string{
-			"Accept":        {"application/json"},
-			"Authorization": {"Bearer " + authSessionId},
-		},
-	}
-
-	response, err := c.HttpClient.Do(req)
-	if err != nil {
-		log.Fatalf("error sending request to business objects. %+v", err)
-	}
-	if !isSuccessStatusCode(response.StatusCode) {
-		return FullCustomModelDto{}, errors.New("http request failed with status code: " + strconv.Itoa(response.StatusCode))
-	}
-	defer response.Body.Close()
-
-	model := FullCustomModelDto{}
-	err = json.NewDecoder(response.Body).Decode(&model)
-	if err != nil {
-		return FullCustomModelDto{}, fmt.Errorf("error parsing response from business objects get custom models - error: %v", err.Error())
-	}
-
-	return model, nil
+type CreateCustomModelDtoEntityType struct {
+	Name        string                       `json:"name"`
+	Description string                       `json:"description"`
+	PluralName  string                       `json:"pluralName"`
+	Key         CreateCustomModelDtoKey      `json:"key"`
+	Properties  CreateCustomModelDtoProperty `json:"properties,omitempty"`
 }
 
 type CreateCustomModelDto struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	State       string `json:"state"`
-	EntityTypes []struct {
-		Name        string `json:"name"`
-		Description string `json:"description"`
-		PluralName  string `json:"pluralName"`
-		Key         struct {
-			Name        string `json:"name"`
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"key"`
-		Properties []struct {
-			Name        string `json:"name"`
-			Description string `json:"description"`
-			Type        string `json:"type"`
-			Required    bool   `json:"required"`
-			Indexed     bool   `json:"indexed"`
-		} `json:"properties,omitempty"`
-	} `json:"entityTypes,omitempty"`
+	Name        string                           `json:"name"`
+	Description string                           `json:"description"`
+	State       string                           `json:"state"`
+	EntityTypes []CreateCustomModelDtoEntityType `json:"entityTypes,omitempty"`
 }
 
 type CreateCustomModelRequest struct {
@@ -192,22 +204,31 @@ func (c *DefaultClient) CreateCustomModel(ctx context.Context, request CreateCus
 	}
 
 	response, err := c.HttpClient.Do(req)
-	if err != nil {
+	if err != nil { // network,timout,etc - not bad status code
 		return "", err
 	}
-	if !isSuccessStatusCode(response.StatusCode) {
-		return "", errors.New("http request failed with status code: " + strconv.Itoa(response.StatusCode))
-	}
-	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		return "", err
 	}
+	defer response.Body.Close()
+
 	stringBody := string(body)
 	fmt.Println(stringBody)
 
-	model := FullCustomModelDto{}
+	if !isSuccessStatusCode(response.StatusCode) {
+
+		errResponseModel := BusinessObjectsErrorResponse{}
+		err = json.Unmarshal(body, &errResponseModel)
+		if err != nil {
+			return "", fmt.Errorf("error parsing error-response from business objects - error: %v", err.Error())
+		}
+
+		return "", errResponseModel.Error
+	}
+
+	model := CustomModel{}
 	err = json.Unmarshal(body, &model)
 	if err != nil {
 		return "", fmt.Errorf("error parsing response from business objects create custom models - error: %v", err.Error())
@@ -216,31 +237,37 @@ func (c *DefaultClient) CreateCustomModel(ctx context.Context, request CreateCus
 	return model.Id, nil
 }
 
-type UpdateCustomModelDto struct {
+type UpdateCustomModelDtoProperty struct {
 	Id          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
-	State       string `json:"state"`
-	EntityTypes []struct {
-		PluralName  string `json:"pluralName"`
-		Id          string `json:"id"`
-		Name        string `json:"name"`
-		Description string `json:"description"`
-		Key         struct {
-			Id          string `json:"id"`
-			Name        string `json:"name"`
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"key"`
-		Properties []struct {
-			Id          string `json:"id"`
-			Name        string `json:"name"`
-			Description string `json:"description"`
-			Type        string `json:"type"`
-			Required    bool   `json:"required"`
-			Indexed     bool   `json:"indexed"`
-		} `json:"properties,omitempty"`
-	} `json:"entityTypes,omitempty"`
+	Type        string `json:"type"`
+	Required    bool   `json:"required"`
+	Indexed     bool   `json:"indexed"`
+}
+
+type UpdateCustomModelDtoKey struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+}
+
+type UpdateCustomModelDtoEntityType struct {
+	Id          string                       `json:"id"`
+	Name        string                       `json:"name"`
+	Description string                       `json:"description"`
+	PluralName  string                       `json:"pluralName"`
+	Key         CreateCustomModelDtoKey      `json:"key"`
+	Properties  CreateCustomModelDtoProperty `json:"properties,omitempty"`
+}
+
+type UpdateCustomModelDto struct {
+	Id          string                           `json:"id"`
+	Name        string                           `json:"name"`
+	Description string                           `json:"description"`
+	State       string                           `json:"state"`
+	EntityTypes []CreateCustomModelDtoEntityType `json:"entityTypes,omitempty"`
 }
 
 type UpdateCustomModelRequest struct {
